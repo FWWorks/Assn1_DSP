767c08ecb0f2e6e9a5365da11953cb1c6a9b9f06
FWWorks <214731923@qq.com>
2019-01-26 15:30:29 -0600

    add methods

--- application/pub.py
+++ application/pub.py
@@ -1,2 +1,30 @@
 
-class Publisher:pass
+class Publisher:
+    def __init__(self, ip_address = None, strength = 0):
+        self.ip_address = ip_address
+        self.strength = strength
+        return 0
+
+    def publish(self, topic, value):
+        return 0
+
+    def register(self, topic):
+        return 0
+
+    def unregister(self, topic):
+        return 0
+
+    def drop_system(self):
+        return 0
+
+    def get_ip_address(self):
+        return self.ip_address
+
+    def set_ip_address(self, ip_address):
+        self.ip_address = ip_address
+        return 0
+
+    def set_strength(self, strength):
+        self.strength = strength
+        return 0
+
--- middleware/pub.py
+++ middleware/pub.py
@@ -1,6 +1,22 @@
+import zmq
+class PublisherDirectly:
+    def __init__(self, ip_address, strength = None):
+        self.ip_address = ip_address
+        self.strength = strength
 
-class PublisherType1:
-    pass
+    def publish(self, ip_address, topic, value):
+        return 0
 
-class PublisherType2:
+    def register(self):
+        return 0
+
+    def unregister(self):
+        return 0
+
+    def drop_system(self):
+        return 0
+
+
+
+class PublisherViaBroker:
     pass

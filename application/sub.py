f057936dfd623b54f43a2322804936a0b1a059f1
Zhehao Zeng <zhehao.zeng@vanderbilt.edu>
2019-01-26 17:02:34 -0600

    method

--- application/sub.py
+++ application/sub.py
@@ -1,3 +1,30 @@
+from middleware.subscriber_mid import *
+
 
 class Subscriber:
+
+    def __init__(self, ip_self, ip_broker):
+        self.ip = ip_self
+        self.ip_b = ip_broker
+        self.sub_mid = Subscriber1(self.ip, self.ip_b)
     pass
+
+    def register(self, topic):
+        self.sub_mid.register(topic)
+    pass
+
+    def receive(self):
+        print(sub_receive())
+        self.sub_mid.receive()
+    pass
+
+    def unregister(self, topic):
+        self.sub_mid.unregister(topic)
+    pass
+
+    def exit(self):
+        self.sub_mid.exit()
+    pass
+
+
+pass

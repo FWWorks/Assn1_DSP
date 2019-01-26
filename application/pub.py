878117156c01324a3d456a79858bd0e48831ae88
FWWorks <214731923@qq.com>
2019-01-26 16:23:30 -0600

    add methods

--- application/pub.py
+++ application/pub.py
@@ -1,14 +1,22 @@
+from middleware.pub import *
 
 class Publisher:
-    def __init__(self, ip_address = None, strength = 0):
+    def __init__(self, mode, ip_address=None, strength=0):
         self.ip_address = ip_address
         self.strength = strength
-        return 0
+        if mode == 1:
+            self.pub_mw = PublisherDirectly(self.ip_address)
+        elif mode == 2:
+            self.pub_mw = PublisherViaBroker()
+        else:
+            print ("mode error, please choose approach")
+
 
     def publish(self, topic, value):
         return 0
 
     def register(self, topic):
+        self.pub_mw.register()
         return 0
 
     def unregister(self, topic):
@@ -20,6 +28,9 @@ class Publisher:
     def get_ip_address(self):
         return self.ip_address
 
+    def get_strength(self):
+        return self.strength
+
     def set_ip_address(self, ip_address):
         self.ip_address = ip_address
         return 0
@@ -27,4 +38,8 @@ class Publisher:
     def set_strength(self, strength):
         self.strength = strength
         return 0
+
+
+p = Publisher(1, "tcp://127.0.0.1:5000")
+p.pub_mw.register()
 
--- middleware/pub.py
+++ middleware/pub.py
@@ -1,13 +1,28 @@
 import zmq
+import json
+
 class PublisherDirectly:
-    def __init__(self, ip_address, strength = None):
+    def __init__(self, ip_address, broker_address, strength=None):
         self.ip_address = ip_address
         self.strength = strength
+        self.socket = ""
+        self.broker_address = broker_address
+        self.socket_broker = None
 
-    def publish(self, ip_address, topic, value):
+
+    def publish(self, topic, value):
+
+
+
         return 0
 
-    def register(self):
+    def register(self, topic):
+        context = zmq.Context()
+        self.socket = context.socket(zmq.PUB)
+        self.socket.bind(self.ip_address)
+        socket_broker = context.socket(zmq.REQ)
+        socket_broker.connect(self.broker_address)
+        socket_broker.send_json((json.dumps({'type': 'add_publisher', 'ip':self.ip_address, 'topic': topic})))
         return 0
 
     def unregister(self):

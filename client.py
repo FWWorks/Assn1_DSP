9c71a88f8147488b9d533210f3142c7aa7d1426c
sudmat <dingjiesu@163.com>
2019-01-27 15:06:38 -0600

    fix bugs in subscriber

--- client.py
+++ client.py
@@ -2,7 +2,7 @@ import time
 import zmq
 context = zmq.Context()
 socket = context.socket(zmq.SUB)
-socket.connect("tcp://127.0.0.1:5000")
-socket.setsockopt(zmq.SUBSCRIBE,'')
+socket.connect("tcp://localhost:5556")
+# socket.setsockopt(zmq.SUBSCRIBE,'')
 while True:
-    print (socket.recv())
+    print (socket.recv_string())

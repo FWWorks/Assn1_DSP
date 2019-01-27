33384b8ab72240d6238ba5e286506b2453a0a5db
FWWorks <214731923@qq.com>
2019-01-27 14:58:55 -0600

    fix a bug

--- client.py
+++ client.py
@@ -3,6 +3,6 @@ import zmq
 context = zmq.Context()
 socket = context.socket(zmq.SUB)
 socket.connect("tcp://127.0.0.1:5000")
-socket.setsockopt(zmq.SUBSCRIBE,'')
+socket.setsockopt_string(zmq.SUBSCRIBE,'')
 while True:
-    print (socket.recv())
+    print (socket.recv_string())
--- server.py
+++ server.py
@@ -3,6 +3,6 @@ context = zmq.Context()
 socket = context.socket(zmq.PUB)
 socket.bind("tcp://127.0.0.1:5000")
 while True:
-    msg = raw_input('input data:')
-    socket.send(msg)
+    msg = input('input data:')
+    socket.send_string(msg)
 

30798f55229206b54068161d3538a29111af0b02
sudmat <dingjiesu@163.com>
2019-02-03 17:02:37 -0600

    1 add logger.py

---
+++ logger.py
@@ -0,0 +1,26 @@
+import logging
+
+def get_logger(log_file):
+
+    # create logger
+    logger_name = log_file
+    logger = logging.getLogger(logger_name)
+    logger.setLevel(logging.INFO)
+
+    # create file handler
+    log_path = log_file
+    fh = logging.FileHandler(log_path, 'w')
+    sh = logging.StreamHandler()
+
+    # create formatter
+    # fmt = "%(asctime)-15s %(levelname)s %(filename)s %(lineno)d %(process)d %(message)s"
+    fmt = "%(asctime)-15s %(levelname)s %(message)s"
+    datefmt = "%Y-%m-%d %H:%M:%S"
+    formatter = logging.Formatter(fmt, datefmt)
+
+    # add handler and formatter to logger
+    fh.setFormatter(formatter)
+    sh.setFormatter(formatter)
+    logger.addHandler(fh)
+    logger.addHandler(sh)
+    return logger
